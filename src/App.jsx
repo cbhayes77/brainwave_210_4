f6a847291beed9df4d38e529fe28ed16733752eb
cbhayes77 <christopher.brandon.hayes@gmail.com>
2025-11-03 20:43:12 -0600

    Worked on Home.jsx. Built part of the hero banner. Ended right before the social icons and headshot.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import Navbar from "./components/layouts/Navbar";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar.jsx";
 
-export default function AppLayout() {
+export default function App() {
   return (
     <div className="min-h-dvh flex flex-col">
       <Navbar />
       {/* Main content goes here */}
+      <main id="main-content" className="flex-1">
+       <Outlet />
+      </main>
+      {/* Footer goes below */}
     </div>
   );
 }
--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Container from "../ui/Container.jsx";
+import Button from "../ui/Button.jsx";
 import { NavLink } from "react-router-dom";
 
 const links = [
@@ -39,7 +40,9 @@ export default function Navbar() {
         </nav>
         {/* Right: CTA / mobile button */}
         <div className="flex-1 flex justify-end">
-            <a href="/contact" className="text-white/80">Contact Me</a>
+            <Button as="a" href="/contact" variant="primary" size="sm" className="hidden md:inline-flex">
+              Contact Me
+            </Button>
         </div>
       </Container>
     </header>
